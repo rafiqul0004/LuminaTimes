49a059d27a5261a193acc079c2d66a157479f777
rafiqul0004 <kr.rafiqul@gmail.com>
2024-01-31 18:54:32 +0600

    Mail update

--- Newspaper/settings.py
+++ Newspaper/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-CSRF_TRUSTED_ORIGINS = ['','https://*.127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://luminatimes.onrender.com','https://*.127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
--- user/views.py
+++ user/views.py
@@ -33,7 +33,7 @@ class UserRegistrationApiView(APIView):
             user=serializer.save()
             token=default_token_generator.make_token(user)
             uid=urlsafe_base64_encode(force_bytes(user.pk))
-            confirm_link=f"http://127.0.0.1:8000/patient/active/{uid}/{token}"
+            confirm_link=f"https://luminatimes.onrender.com/user/active/{uid}/{token}"
             email_subject="Confirm Your Email"
             email_body=render_to_string('confirm_email.html',{'confirm_link':confirm_link})
             email=EmailMultiAlternatives(email_subject,'',to=[user.email])
