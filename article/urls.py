2701603e9d2dfce0971b2c50a28f54431172ccef
rafiqul0004 <kr.rafiqul@gmail.com>
2024-01-31 19:30:31 +0600

    Used

--- article/urls.py
+++ article/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .import views
 # Create a router and register our ViewSets with it.
 router = DefaultRouter()
-router.register('article', views.ArticleViewSet)
+router.register('article_list', views.ArticleViewSet)
 router.register('category', views.CategoryViewSet)
 router.register('review', views.ReviewViewSet)
 
